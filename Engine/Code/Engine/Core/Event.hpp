907baaaeff9d55555fde8642992d909662255f08
cugone <cugone@gmail.com>
2020-01-10 08:48:19 -0600

    Pointer types have their asterisks next to the type

--- Engine/Code/Engine/Core/Event.hpp
+++ Engine/Code/Engine/Core/Event.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include <vector>
 
 template <typename ...ARGS>
@@ -11,14 +12,14 @@ public:
 
     struct event_sub_t {
         cb_t cb;
-        void *secondary_cb;
-        void *user_arg;
+        void* secondary_cb;
+        void* user_arg;
     };
 
     Event() = default;
     ~Event() = default;
 
-    void Subscribe(void *user_arg, cb_with_arg_t cb) noexcept {
+    void Subscribe(void* user_arg, cb_with_arg_t cb) noexcept {
         event_sub_t sub;
         sub.cb = FunctionWithArgumentCallback;
         sub.secondary_cb = cb;
@@ -26,7 +27,7 @@ public:
         subscriptions.push_back(sub);
     }
 
-    void Unsubscribe(void *user_arg, void* cb) noexcept {
+    void Unsubscribe(void* user_arg, void* cb) noexcept {
         subscriptions.erase(std::remove_if(std::begin(subscriptions),
             std::end(subscriptions),
             [&cb, &user_arg](const event_sub_t& sub) {
@@ -35,7 +36,7 @@ public:
             std::end(subscriptions));
     }
 
-    void Unsubscribe_by_argument(void *user_arg) noexcept {
+    void Unsubscribe_by_argument(void* user_arg) noexcept {
         subscriptions.erase(std::remove_if(std::begin(subscriptions),
             std::end(subscriptions),
             [&user_arg](const event_sub_t& sub) {
@@ -45,7 +46,7 @@ public:
     }
 
     template <typename T>
-    void Subscribe_method(T *obj, void (T::*mcb)(ARGS...)) noexcept {
+    void Subscribe_method(T* obj, void (T::*mcb)(ARGS...)) noexcept {
         event_sub_t sub;
         sub.cb = MethodCallback<T, decltype(mcb)>;
         sub.secondary_cb = *(void**)(&mcb);
@@ -54,12 +55,12 @@ public:
     }
 
     template <typename T>
-    void Unsubscribe_method(T *obj, void (T::*mcb)(ARGS...)) noexcept {
+    void Unsubscribe_method(T* obj, void (T::*mcb)(ARGS...)) noexcept {
         Unsubscribe(obj, *(void**)&mcb);
     }
 
     template <typename T>
-    void Unsubscribe_object(T *obj) noexcept {
+    void Unsubscribe_object(T* obj) noexcept {
         Unsubscribe_by_argument(obj);
     }
 
@@ -78,23 +79,23 @@ public:
 private:
     std::vector<event_sub_t> subscriptions;
 
-    static void FunctionWithArgumentCallback(event_sub_t *sub, ARGS ...args) noexcept;
+    static void FunctionWithArgumentCallback(event_sub_t* sub, ARGS ...args) noexcept;
 
     template <typename T, typename MCB>
-    static void MethodCallback(event_sub_t *sub, ARGS ...args) noexcept;
+    static void MethodCallback(event_sub_t* sub, ARGS ...args) noexcept;
 
 };
 
 template <typename ...ARGS>
-void Event<ARGS...>::FunctionWithArgumentCallback(event_sub_t *sub, ARGS ...args) noexcept {
+void Event<ARGS...>::FunctionWithArgumentCallback(event_sub_t* sub, ARGS ...args) noexcept {
     cb_with_arg_t cb = (cb_with_arg_t)(sub->secondary_cb);
     cb(sub->user_arg, args...);
 }
 
 template <typename ...ARGS>
 template <typename T, typename MCB>
-void Event<ARGS...>::MethodCallback(event_sub_t *sub, ARGS ...args) noexcept {
+void Event<ARGS...>::MethodCallback(event_sub_t* sub, ARGS ...args) noexcept {
     MCB mcb = *(MCB*)&(sub->secondary_cb);
-    T *obj = (T*)(sub->user_arg);
+    T* obj = (T*)(sub->user_arg);
     (obj->*mcb)(args...);
 }
