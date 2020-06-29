164bc561e1f386936cde965bde6dc2788bc0b8ef
cugone <cugone@gmail.com>
2020-06-29 18:45:49 -0500

    Implemented OrthographicCameraController

---
+++ Engine/Code/Engine/Core/OrthographicCameraController.cpp
@@ -0,0 +1,103 @@
+#include "Engine/Core/OrthographicCameraController.hpp"
+
+#include "Engine/Input/InputSystem.hpp"
+#include "Engine/Renderer/Renderer.hpp"
+
+#include <algorithm>
+
+OrthographicCameraController::OrthographicCameraController(Renderer* renderer, InputSystem* inputSystem, float aspectRatio /*= 1.777778f*/) noexcept
+: m_renderer(renderer)
+, m_inputSystem(inputSystem)
+, m_aspectRatio(aspectRatio)
+{
+    /* DO NOTHING */
+}
+
+void OrthographicCameraController::Update([[maybe_unused]] TimeUtils::FPSeconds deltaSeconds) noexcept {
+    if(m_inputSystem->IsKeyDown(KeyCode::RButton)) {
+        const auto up = -Vector2::Y_AXIS * m_translationSpeed * deltaSeconds.count();
+        const auto down = -up;
+        const auto left = -Vector2::X_AXIS * m_translationSpeed * deltaSeconds.count();
+        const auto right = -left;
+        if(m_inputSystem->IsKeyDown(KeyCode::W)) {
+            Translate(up);
+        } else if(m_inputSystem->IsKeyDown(KeyCode::S)) {
+            Translate(down);
+        }
+        if(m_inputSystem->IsKeyDown(KeyCode::A)) {
+            Translate(left);
+        } else if(m_inputSystem->IsKeyDown(KeyCode::D)) {
+            Translate(right);
+        }
+        if(m_inputSystem->WasMouseWheelJustScrolledUp()) {
+            m_zoomLevel -= (m_zoomSpeed / m_maxZoomSpeed) * MathUtils::EasingFunctions::SmoothStop<3>(deltaSeconds.count());
+        } else if(m_inputSystem->WasMouseWheelJustScrolledDown()) {
+            m_zoomLevel += (m_zoomSpeed / m_maxZoomSpeed) * MathUtils::EasingFunctions::SmoothStop<3>(deltaSeconds.count());
+        }
+    }
+    m_zoomLevel = std::clamp(m_zoomLevel, m_minZoomLevel, m_maxZoomLevel);
+    m_Camera.Update(deltaSeconds);
+    m_Camera.SetupView(Vector2{-m_aspectRatio * m_zoomLevel, m_zoomLevel}, Vector2{m_aspectRatio * m_zoomLevel, -m_zoomLevel}, Vector2{0.0f, 1.0f}, m_aspectRatio);
+    m_renderer->SetCamera(m_Camera);
+}
+
+void OrthographicCameraController::SetAspectRatio(float aspectRatio) noexcept {
+    m_aspectRatio = aspectRatio;
+}
+
+float OrthographicCameraController::GetAspectRatio() const noexcept {
+    return m_aspectRatio;
+}
+
+void OrthographicCameraController::SetPosition(const Vector2& newPosition) noexcept {
+    m_Camera.SetPosition(newPosition);
+}
+
+void OrthographicCameraController::SetRotationDegrees(float newRotation) noexcept {
+    m_Camera.SetOrientationDegrees(newRotation);
+}
+
+void OrthographicCameraController::SetRotationRadians(float newRotation) noexcept {
+    m_Camera.SetOrientation(newRotation);
+}
+
+void OrthographicCameraController::Translate(const Vector2& offset) noexcept {
+    m_Camera.Translate(offset / m_zoomLevel);
+}
+
+void OrthographicCameraController::RotateDegrees(float offset) noexcept {
+    m_Camera.ApplyOrientationDegrees(offset);
+}
+
+void OrthographicCameraController::RotateRadians(float offset) noexcept {
+    m_Camera.ApplyOrientation(offset);
+}
+
+void OrthographicCameraController::ResetZoomLevelRange() noexcept {
+    SetZoomLevelRange(Vector2{m_defaultMinZoomLevel, m_defaultMaxZoomLevel});
+}
+
+void OrthographicCameraController::SetZoomLevel(float zoom) noexcept {
+    m_zoomLevel = zoom;
+}
+
+void OrthographicCameraController::SetZoomLevelRange(const Vector2& minmaxZoomLevel) noexcept {
+    SetMinZoomLevel(minmaxZoomLevel.x);
+    SetMaxZoomLevel(minmaxZoomLevel.y);
+}
+
+void OrthographicCameraController::SetMinZoomLevel(float minimumLevel) noexcept {
+    m_minZoomLevel = (std::max)(1.0f, minimumLevel);
+}
+
+void OrthographicCameraController::SetMaxZoomLevel(float maximumValue) noexcept {
+    m_maxZoomLevel = maximumValue;
+}
+
+const Camera2D& OrthographicCameraController::GetCamera() const noexcept {
+    return m_Camera;
+}
+
+Camera2D& OrthographicCameraController::GetCamera() noexcept {
+    return m_Camera;
+}
---
+++ Engine/Code/Engine/Core/OrthographicCameraController.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "Engine/Core/TimeUtils.hpp"
+#include "Engine/Math/MathUtils.hpp"
+#include "Engine/Renderer/Camera2D.hpp"
+
+class Renderer;
+class InputSystem;
+
+class OrthographicCameraController {
+public:
+    OrthographicCameraController() noexcept = default;
+    explicit OrthographicCameraController(Renderer* renderer, InputSystem* inputSystem, float aspectRatio = 1.777778f) noexcept;
+
+    void Update([[maybe_unused]] TimeUtils::FPSeconds deltaSeconds) noexcept;
+
+    void SetAspectRatio(float aspectRatio) noexcept;
+    float GetAspectRatio() const noexcept;
+
+    void SetPosition(const Vector2& newPosition) noexcept;
+    void Translate(const Vector2& offset) noexcept;
+    
+    void SetRotationDegrees(float newRotation) noexcept;
+    void SetRotationRadians(float newRotation) noexcept;
+    void RotateDegrees(float offset) noexcept;
+    void RotateRadians(float offset) noexcept;
+
+    void ResetZoomLevelRange() noexcept;
+    void SetZoomLevel(float zoom) noexcept;
+    void SetZoomLevelRange(const Vector2& minmaxZoomLevel) noexcept;
+    void SetMinZoomLevel(float minimumLevel) noexcept;
+    void SetMaxZoomLevel(float maximumValue) noexcept;
+
+    const Camera2D& GetCamera() const noexcept;
+    Camera2D& GetCamera() noexcept;
+
+protected:
+private:
+    Renderer* m_renderer{};
+    InputSystem* m_inputSystem{};
+    float m_aspectRatio = MathUtils::M_16_BY_9_RATIO;
+    float m_zoomLevel = 8.0f;
+    float m_defaultMinZoomLevel = 8.0f;
+    float m_defaultMaxZoomLevel = (std::numeric_limits<float>::max)();
+    float m_minZoomLevel = 8.0f;
+    float m_maxZoomLevel = (std::numeric_limits<float>::max)();
+    Camera2D m_Camera{};
+    float m_translationSpeed = 5.0f;
+    float m_rotationSpeed = 180.0f;
+    float m_zoomSpeed = 8.0f;
+    float m_maxZoomSpeed = 24.0f;
+};
