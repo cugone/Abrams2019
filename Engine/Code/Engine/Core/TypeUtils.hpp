5146bb7a233dc0c8eb8ce8a8f186afe5bc869fb3
cugone <cugone@gmail.com>
2020-07-16 02:37:14 -0500

    Move non-public types into detail namespace

--- Engine/Code/Engine/Core/TypeUtils.hpp
+++ Engine/Code/Engine/Core/TypeUtils.hpp
@@ -6,57 +6,59 @@
 namespace TypeUtils {
 
     template<typename E>
-    using is_scoped_enum = std::integral_constant<bool, std::is_enum<E>::value && !std::is_convertible<E, int>::value>;
-
-    template<typename E>
-    constexpr bool is_scoped_enum_v = is_scoped_enum<E>::value;
-
-    template<typename E>
     struct is_bitflag_enum_type : std::false_type {};
-
-    template<typename E>
-    constexpr bool is_bitflag_enum_type_v = is_bitflag_enum_type<E>::value;
-
-    template<typename E>
-    using is_bitflag_enum = std::integral_constant<bool, is_scoped_enum_v<E>&& is_bitflag_enum_type_v<E>>;
-
-    template<typename E>
-    constexpr bool is_bitflag_enum_v = is_bitflag_enum<E>::value;
-
 
     template<typename E>
     struct is_incrementable_enum_type : std::false_type {};
 
     template<typename E>
-    constexpr bool is_incrementable_enum_type_v = is_incrementable_enum_type<E>::value;
-
-    template<typename E>
-    using is_incrementable_enum = std::integral_constant<bool, is_scoped_enum_v<E>&& is_incrementable_enum_type_v<E>>;
-
-    template<typename E>
-    constexpr bool is_incrementable_enum_v = is_incrementable_enum<E>::value;
-
-
-    template<typename E>
     struct is_decrementable_enum_type : std::false_type {};
 
-    template<typename E>
-    constexpr bool is_decrementable_enum_type_v = is_decrementable_enum_type<E>::value;
-
-    template<typename E>
-    using is_decrementable_enum = std::integral_constant<bool, is_scoped_enum_v<E>&& is_decrementable_enum_type_v<E>>;
-
-    template<typename E>
-    constexpr bool is_decrementable_enum_v = is_decrementable_enum<E>::value;
-
     namespace detail {
-        template<typename E, typename = std::enable_if_t<TypeUtils::is_scoped_enum_v<E>>>
+
+        template<typename E>
+        using is_scoped_enum = std::integral_constant<bool, std::is_enum<E>::value && !std::is_convertible<E, int>::value>;
+
+        template<typename E>
+        constexpr bool is_scoped_enum_v = detail::is_scoped_enum<E>::value;
+
+        template<typename E>
+        constexpr bool is_bitflag_enum_type_v = is_bitflag_enum_type<E>::value;
+
+        template<typename E>
+        using is_bitflag_enum = std::integral_constant<bool, is_scoped_enum_v<E> && is_bitflag_enum_type_v<E>>;
+
+        template<typename E>
+        constexpr bool is_bitflag_enum_v = is_bitflag_enum<E>::value;
+
+
+        template<typename E>
+        constexpr bool is_incrementable_enum_type_v = is_incrementable_enum_type<E>::value;
+
+        template<typename E>
+        using is_incrementable_enum = std::integral_constant<bool, is_scoped_enum_v<E> && is_incrementable_enum_type_v<E>>;
+
+        template<typename E>
+        constexpr bool is_incrementable_enum_v = is_incrementable_enum<E>::value;
+
+
+        template<typename E>
+        constexpr bool is_decrementable_enum_type_v = is_decrementable_enum_type<E>::value;
+
+        template<typename E>
+        using is_decrementable_enum = std::integral_constant<bool, is_scoped_enum_v<E> && is_decrementable_enum_type_v<E>>;
+
+        template<typename E>
+        constexpr bool is_decrementable_enum_v = is_decrementable_enum<E>::value;
+
+
+        template<typename E, typename = std::enable_if_t<TypeUtils::detail::is_scoped_enum_v<E>>>
         const auto GetUnderlyingValue(E a) {
             using underlying = std::underlying_type_t<E>;
             return static_cast<underlying>(a);
         }
 
-        template<typename E, typename = std::enable_if_t<TypeUtils::is_scoped_enum_v<E>>>
+        template<typename E, typename = std::enable_if_t<TypeUtils::detail::is_scoped_enum_v<E>>>
         const auto GetUnderlyingValues(E a, E b) {
             using underlying = std::underlying_type_t<E>;
             return std::make_pair(static_cast<underlying>(a), static_cast<underlying>(b));
@@ -70,46 +72,46 @@ namespace TypeUtils {
 /* BITFLAGS                                                             */
 /************************************************************************/
 
-template<typename E, typename = std::enable_if_t<TypeUtils::is_bitflag_enum_v<E>>>
+template<typename E, typename = std::enable_if_t<TypeUtils::detail::is_bitflag_enum_v<E>>>
 E& operator|=(E& a, const E& b) noexcept {
     const auto [underlying_a, underlying_b] = TypeUtils::detail::GetUnderlyingValues(a, b);
     a = static_cast<E>(underlying_a | underlying_b);
     return a;
 }
 
-template<typename E, typename = std::enable_if_t<TypeUtils::is_bitflag_enum_v<E>>>
+template<typename E, typename = std::enable_if_t<TypeUtils::detail::is_bitflag_enum_v<E>>>
 E operator|(E a, const E& b) noexcept {
     a |= b;
     return a;
 }
 
-template<typename E, typename = std::enable_if_t<TypeUtils::is_bitflag_enum_v<E>>>
+template<typename E, typename = std::enable_if_t<TypeUtils::detail::is_bitflag_enum_v<E>>>
 E& operator&=(E& a, const E& b) noexcept {
     const auto [underlying_a, underlying_b] = TypeUtils::detail::GetUnderlyingValues(a, b);
     a = static_cast<E>(underlying_a & underlying_b);
     return a;
 }
 
-template<typename E, typename = std::enable_if_t<TypeUtils::is_bitflag_enum_v<E>>>
+template<typename E, typename = std::enable_if_t<TypeUtils::detail::is_bitflag_enum_v<E>>>
 E operator&(E a, const E& b) noexcept {
     a &= b;
     return a;
 }
 
-template<typename E, typename = std::enable_if_t<TypeUtils::is_bitflag_enum_v<E>>>
+template<typename E, typename = std::enable_if_t<TypeUtils::detail::is_bitflag_enum_v<E>>>
 E& operator^=(E& a, const E& b) noexcept {
     const auto [underlying_a, underlying_b] = TypeUtils::detail::GetUnderlyingValues(a, b);
     a = static_cast<E>(underlying_a ^ underlying_b);
     return a;
 }
 
-template<typename E, typename = std::enable_if_t<TypeUtils::is_bitflag_enum_v<E>>>
+template<typename E, typename = std::enable_if_t<TypeUtils::detail::is_bitflag_enum_v<E>>>
 E operator^(E a, const E& b) noexcept {
     a ^= b;
     return a;
 }
 
-template<typename E, typename = std::enable_if_t<TypeUtils::is_bitflag_enum_v<E>>>
+template<typename E, typename = std::enable_if_t<TypeUtils::detail::is_bitflag_enum_v<E>>>
 E operator~(E a) noexcept {
     const auto underlying_a = TypeUtils::detail::GetUnderlyingValue(a);
     a = static_cast<E>(~underlying_a);
@@ -120,7 +122,7 @@ E operator~(E a) noexcept {
 /* INCREMENTABLE                                                        */
 /************************************************************************/
 
-template<typename E, typename = std::enable_if_t<TypeUtils::is_incrementable_enum_v<E>>>
+template<typename E, typename = std::enable_if_t<TypeUtils::detail::is_incrementable_enum_v<E>>>
 E& operator++(E& e) noexcept {
     using underlying = std::underlying_type_t<E>;
     e = static_cast<E>(static_cast<underlying>(e) + 1);
@@ -130,7 +132,7 @@ E& operator++(E& e) noexcept {
     return e;
 }
 
-template<typename E, typename = std::enable_if_t<TypeUtils::is_incrementable_enum_v<E>>>
+template<typename E, typename = std::enable_if_t<TypeUtils::detail::is_incrementable_enum_v<E>>>
 E operator++(E& e, int) noexcept {
     E result = e;
     ++e;
@@ -142,7 +144,7 @@ E operator++(E& e, int) noexcept {
 /* DECREMENTABLE                                                        */
 /************************************************************************/
 
-template<typename E, typename = std::enable_if_t<TypeUtils::is_decrementable_enum_v<E>>>
+template<typename E, typename = std::enable_if_t<TypeUtils::detail::is_decrementable_enum_v<E>>>
 E& operator--(E& e) noexcept {
     if(e == E::First_) {
         e = E::Last_;
@@ -152,7 +154,7 @@ E& operator--(E& e) noexcept {
     return e;
 }
 
-template<typename E, typename = std::enable_if_t<TypeUtils::is_decrementable_enum_v<E>>>
+template<typename E, typename = std::enable_if_t<TypeUtils::detail::is_decrementable_enum_v<E>>>
 E operator--(E& e, int) noexcept {
     E result = e;
     --e;
