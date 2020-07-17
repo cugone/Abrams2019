8104c35f7a6792f68a13adeceb7a167038c90e21
cugone <cugone@gmail.com>
2020-07-17 13:52:52 -0500

    Added Utilities files
    Implemented DoOnce
    Implemented FlipFlop

---
+++ Engine/Code/Engine/Core/Utilities.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <type_traits>
+
+namespace Utils {
+
+    template<typename Callable>
+    void DoOnce(Callable&& f, bool reset = false, bool start_closed = false) {
+        static bool once = start_closed;
+        if(!once) {
+            once = true;
+            std::invoke(f);
+        }
+        if(reset) {
+            once = false;
+        }
+    }
+
+    template<typename CallableT, typename CallableF>
+    bool FlipFlop(CallableT&& f_true, CallableF&& f_false) {
+        static bool value = true;
+        bool is_true = value;
+        value ? std::invoke(f_true) : std::invoke(f_false);
+        value = !value;
+        return is_true;
+    }
+
+}
+
