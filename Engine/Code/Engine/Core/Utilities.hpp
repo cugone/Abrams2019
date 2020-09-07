689474b1b94fd55569fb022039f9307d70c599c7
cugone <cugone@gmail.com>
2020-09-07 10:56:10 -0500

    Added degenerate FlipFlop function

--- Engine/Code/Engine/Core/Utilities.hpp
+++ Engine/Code/Engine/Core/Utilities.hpp
@@ -17,12 +17,16 @@ namespace Utils {
     }
 
     template<typename CallableT, typename CallableF>
-    bool FlipFlop(CallableT&& f_true, CallableF&& f_false) {
+    bool FlipFlop(CallableT&& f_true, CallableF&& f_false) noexcept {
         static bool value = true;
         bool is_true = value;
         value ? std::invoke(f_true) : std::invoke(f_false);
         value = !value;
         return is_true;
+    }
+
+    bool FlipFlop() noexcept {
+        return FlipFlop([]() {}, []() {});
     }
 
 }
