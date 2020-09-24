641eb03042362dac571d416cddef3e5140dc183a
cugone <cugone@gmail.com>
2020-09-24 13:27:57 -0500

    Implemented SpringJoint class

---
+++ Engine/Code/Engine/Physics/SpringJoint.cpp
@@ -0,0 +1,62 @@
+#include "Engine/Physics/SpringJoint.hpp"
+
+#include "Engine/Physics/RigidBody.hpp"
+
+#include "Engine/Renderer/Renderer.hpp"
+
+void SpringJoint::SetAnchors(const Vector2& a, const Vector2& b) noexcept {
+    _anchors = std::make_pair(a, b);
+}
+
+void SpringJoint::SetStiffness(float k) noexcept {
+    _k = k;
+}
+
+void SpringJoint::SetMinimumCompressionDistance(float distance) noexcept {
+    _compressionLength = distance;
+}
+
+void SpringJoint::SetRestingLength(float length) noexcept {
+    _restingLength = length;
+}
+
+void SpringJoint::notify([[maybe_unused]] TimeUtils::FPSeconds deltaSeconds) noexcept {
+    auto* first_body = bodyA;
+    auto* second_body = bodyB;
+    if(first_body == nullptr && second_body == nullptr) {
+        return;
+    }
+
+    const auto fb_pos = first_body == nullptr ? _anchors.first : first_body->GetPosition();
+    const auto sb_pos = second_body == nullptr ? _anchors.second : second_body->GetPosition();
+
+    auto left_direction = fb_pos - sb_pos;
+    auto left_magnitude = left_direction.CalcLength();
+    if(MathUtils::IsEquivalent(std::abs(left_magnitude), _restingLength) == false) {
+        auto current_compression = left_magnitude - _restingLength;
+        if(current_compression <= -_compressionLength)
+            current_compression = -_compressionLength;
+        left_magnitude = _k * current_compression;
+        left_direction.Normalize();
+    }
+
+    auto right_direction = sb_pos - fb_pos;
+    auto right_magnitude = right_direction.CalcLength();
+    if(MathUtils::IsEquivalent(std::abs(right_magnitude), _restingLength) == false) {
+        auto current_compression = right_magnitude - _restingLength;
+        if(current_compression <= -_compressionLength)
+            current_compression = -_compressionLength;
+        right_magnitude = _k * current_compression;
+        right_direction.Normalize();
+    }
+
+    first_body->ApplyForce(right_direction * right_magnitude, TimeUtils::FPSeconds::zero());
+    second_body->ApplyForce(left_direction * left_magnitude, TimeUtils::FPSeconds::zero());
+
+}
+
+void SpringJoint::DebugRender(Renderer& renderer) const noexcept {
+    auto* first_body = bodyA;
+    auto* second_body = bodyB;
+    renderer.DrawLine2D(first_body->GetPosition(), second_body->GetPosition());
+}
---
+++ Engine/Code/Engine/Physics/SpringJoint.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "Engine/Physics/Joint.hpp"
+#include "Engine/Math/Vector2.hpp"
+
+class Renderer;
+
+class SpringJoint : public Joint {
+public:
+    SpringJoint() = default;
+    SpringJoint(const SpringJoint& other) = default;
+    SpringJoint(SpringJoint&& other) = default;
+    SpringJoint& operator=(const SpringJoint& other) = default;
+    SpringJoint& operator=(SpringJoint&& other) = default;
+    virtual ~SpringJoint() = default;
+
+    void SetAnchors(const Vector2& a, const Vector2& b) noexcept;
+    void SetStiffness(float k) noexcept;
+    void SetMinimumCompressionDistance(float distance) noexcept;
+    void SetRestingLength(float length) noexcept;
+
+    void notify([[maybe_unused]] TimeUtils::FPSeconds deltaSeconds) noexcept override;
+    void DebugRender(Renderer& renderer) const noexcept override;
+protected:
+private:
+    float _compressionLength = 0.0f;
+    float _restingLength = 1.0f;
+    float _k = 1.0f;
+    std::pair<Vector2, Vector2> _anchors{};
+};
