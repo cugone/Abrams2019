8366582ebc9ae65f76781d32590b7ab67b0ae008
cugone <cugone@gmail.com>
2020-09-23 21:53:47 -0500

    Implemented ForceGenerator

--- Engine/Code/Engine/Physics/ForceGenerator.cpp
+++ Engine/Code/Engine/Physics/ForceGenerator.cpp
@@ -4,12 +4,16 @@
 
 #include <algorithm>
 
-ForceGenerator::~ForceGenerator() {/* DO NOTHING */}
-
 void ForceGenerator::attach(RigidBody* body) noexcept {
-    _observers.push_back(body);
+    if(!is_attached(body)) {
+        _observers.push_back(body);
+    }
 }
 
 void ForceGenerator::detach(RigidBody* body) noexcept {
     _observers.erase(std::remove_if(std::begin(_observers), std::end(_observers), [body](const RigidBody* a) { return a == body; }), std::end(_observers));
+}
+
+bool ForceGenerator::is_attached(const RigidBody* const body) const noexcept {
+    return std::find(std::cbegin(_observers), std::cend(_observers), body) != std::cend(_observers);
 }
--- Engine/Code/Engine/Physics/ForceGenerator.hpp
+++ Engine/Code/Engine/Physics/ForceGenerator.hpp
@@ -8,11 +8,18 @@ class RigidBody;
 
 class ForceGenerator {
 public:
-    virtual ~ForceGenerator() = 0;
+    ForceGenerator() noexcept = default;
+    ForceGenerator(const ForceGenerator& other) = default;
+    ForceGenerator(ForceGenerator&& other) = default;
+    ForceGenerator& operator=(const ForceGenerator& other) = default;
+    ForceGenerator& operator=(ForceGenerator&& other) = default;
+    virtual ~ForceGenerator() noexcept = default;
 
     void attach(RigidBody* body) noexcept;
     void detach(RigidBody* body) noexcept;
     virtual void notify([[maybe_unused]] TimeUtils::FPSeconds deltaSeconds) const noexcept = 0;
+
+    bool is_attached(const RigidBody* const body) const noexcept;
 
 protected:
     std::vector<RigidBody*> _observers{};
