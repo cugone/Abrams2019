d41b11244c2b08d3ccbbb24020d83ae6749db9d2
cugone <cugone@gmail.com>
2020-09-23 21:54:03 -0500

    Implemented GravityForceGenerator

---
+++ Engine/Code/Engine/Physics/GravityForceGenerator.cpp
@@ -0,0 +1,25 @@
+#include "Engine/Physics/GravityForceGenerator.hpp"
+
+#include "Engine/Physics/RigidBody.hpp"
+
+#include "Engine/Math/Vector2.hpp"
+
+GravityForceGenerator::GravityForceGenerator(const Vector2& gravity) noexcept
+: ForceGenerator()
+, g(gravity)
+{
+    /* DO NOTHING */
+}
+
+void GravityForceGenerator::notify([[maybe_unused]] TimeUtils::FPSeconds deltaSeconds) const noexcept {
+    if(g == Vector2::ZERO) {
+        return;
+    }
+    for(auto* body : _observers) {
+        body->ApplyForce(g, deltaSeconds);
+    }
+}
+
+void GravityForceGenerator::SetGravity(const Vector2& newGravity) noexcept {
+    g = newGravity;
+}
