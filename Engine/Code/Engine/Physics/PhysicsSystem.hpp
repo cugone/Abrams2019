b3369cb8c467545f4d8fef0940b2a169061df12c
cugone <cugone@gmail.com>
2020-10-02 13:06:14 -0500

    Added static_assert to catch invalid types

--- Engine/Code/Engine/Physics/PhysicsSystem.hpp
+++ Engine/Code/Engine/Physics/PhysicsSystem.hpp
@@ -121,6 +121,7 @@ private:
 
 template<typename JointDefType>
 Joint* PhysicsSystem::CreateJoint(const JointDefType& defType) noexcept {
+    static_assert(std::is_base_of_v<JointDef, JointDefType>, "CreateJoint received type not derived from Joint.");
     std::unique_ptr<Joint> newJoint{};
     if constexpr(std::is_same_v<JointDefType, SpringJointDef>) {
         newJoint.reset(new SpringJoint(defType));
