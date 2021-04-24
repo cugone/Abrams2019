ce5d2da338d1dade4c407118ca5565bee763d553
cugone <cugone@gmail.com>
2021-04-24 11:46:26 -0500

    Initialize member variables

--- Engine/Code/Engine/Math/Ray2.hpp
+++ Engine/Code/Engine/Math/Ray2.hpp
@@ -6,8 +6,8 @@
 
 class Ray2 {
 public:
-    Vector2 position;
-    Vector2 direction;
+    Vector2 position{};
+    Vector2 direction{1.0f, 0.0f};
 
     void SetOrientationDegrees(float angleDegrees);
     void SetOrientationRadians(float angleRadians);
