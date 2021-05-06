d23619ca69b5d0f00b3ae65df0752af455d4035e
cugone <cugone@gmail.com>
2021-05-05 20:24:00 -0500

    Removed dead code

--- Engine/Code/Engine/Physics/PhysicsUtils.cpp
+++ Engine/Code/Engine/Physics/PhysicsUtils.cpp
@@ -152,50 +152,6 @@ EPAResult PhysicsUtils::EPA(GJKResult gjk, const Collider& a, const Collider& b)
     }
 
     return {minDistance + epsilon, Vector3{minNormal}};
-    /*
-    using Simplex = std::vector<Vector3>;
-    using Edge = std::tuple<float, Vector3, std::size_t>;
-    auto simplex_copy = gjk.simplex;
-    GUARANTEE_OR_DIE(simplex_copy.size() == 3, "EPA Simplex not a triangle.");
-    const auto findClosestEdge = [](const Simplex& simplex) {
-        float distance = std::numeric_limits<float>::infinity();
-        Edge closest = std::make_tuple(distance, Vector3::ZERO, -1);
-        for(std::size_t i = 0; i < simplex.size(); ++i) {
-            const auto j = (i + 1) % simplex.size();
-            const auto a = simplex[i];
-            const auto b = simplex[j];
-            const auto e = b - a;
-            const auto oa = a;
-            const auto n = MathUtils::TripleProductVector(e, oa, e).GetNormalize();
-            float d = MathUtils::DotProduct(n, a);
-            if(d < distance) {
-                distance = d;
-                std::get<0>(closest) = d;
-                std::get<1>(closest) = n;
-                std::get<2>(closest) = j;
-            }
-        }
-        return closest;
-    };
-    const auto calcMinkowskiDiff = [](const Vector3& direction, const Collider& a) { return a.Support(Vector2{direction}); };
-    const auto support = [&](const Vector3& direction) { return calcMinkowskiDiff(direction, a) - calcMinkowskiDiff(-direction, b); };
-    for(;;) {
-        const auto edge = findClosestEdge(simplex_copy);
-        const auto distance = std::get<0>(edge);
-        const auto n = std::get<1>(edge);
-        GUARANTEE_RECOVERABLE(!MathUtils::IsEquivalentToZero(n), "EPA n was zero.");
-        const auto i = std::get<2>(edge);
-        const auto p = Vector3{support(n)};
-        const auto d = MathUtils::DotProduct(p, Vector3{n});
-        //Is "distance" within some tolerance? i.e. is it "close enough"?
-        //epsilon defaults to 0.00001, which I think is "close enough".
-        if(d - distance < 0.0001f) {
-            return {d, n};
-        } else {
-            simplex_copy.insert(std::begin(simplex_copy) + i, Vector3{p});
-        }
-    }
-    */
 }
 
 bool PhysicsUtils::SAT(const Collider& a, const Collider& b) {
