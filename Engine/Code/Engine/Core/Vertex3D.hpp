2a5f05837e4cdf3325bc7450436c1f0de7e701cf
cugone <cugone@gmail.com>
2021-05-17 15:48:12 -0500

    Moved ctor definition inside class declaration

--- Engine/Code/Engine/Core/Vertex3D.hpp
+++ Engine/Code/Engine/Core/Vertex3D.hpp
@@ -8,6 +8,20 @@ class InputLayout;
 
 struct Vertex3D {
     Vertex3D(const Vector3& pos = Vector3::ZERO, const Rgba& color = Rgba::White, const Vector2& tex_coords = Vector2::ZERO, const Vector3& normal = Vector3::Z_AXIS, const Vector3& tangent = Vector3::ZERO, const Vector3& bitangent = Vector3::ZERO) noexcept;
+    Vertex3D(const Vector3& pos = Vector3::ZERO
+            ,const Rgba& color = Rgba::White
+            ,const Vector2& tex_coords = Vector2::ZERO
+            ,const Vector3& normal = Vector3::Z_AXIS
+            ,const Vector3& tangent = Vector3::ZERO
+            ,const Vector3& bitangent = Vector3::ZERO) noexcept
+    : position(pos)
+    , color(color.GetRgbaAsFloats())
+    , texcoords(tex_coords)
+    , normal(normal)
+    , tangent(tangent)
+    , bitangent(bitangent) {
+        /* DO NOTHING */
+    }
     Vertex3D(const Vertex3D& other) = default;
     Vertex3D(Vertex3D&& other) = default;
     Vertex3D& operator=(const Vertex3D& other) = default;
@@ -22,23 +36,3 @@ struct Vertex3D {
 protected:
 private:
 };
-
-inline Vertex3D::Vertex3D(const Vector3& pos /*= Vector3::ZERO*/
-                          ,
-                          const Rgba& color /*= Rgba::WHITE*/
-                          ,
-                          const Vector2& tex_coords /*= Vector2::ZERO*/
-                          ,
-                          const Vector3& normal /*= Vector3::Z_AXIS*/
-                          ,
-                          const Vector3& tangent /* Vector3::ZERO */
-                          ,
-                          const Vector3& bitangent /* Vector3::ZERO */) noexcept
-: position(pos)
-, color(color.GetRgbaAsFloats())
-, texcoords(tex_coords)
-, normal(normal)
-, tangent(tangent)
-, bitangent(bitangent) {
-    /* DO NOTHING */
-}
