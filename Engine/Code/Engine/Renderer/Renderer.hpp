9827e56cf78e4bbbc99f37d701645c560228472f
cugone <cugone@gmail.com>
2021-06-11 01:46:42 -0500

    auto cannot deduce references
    copy intentional, but silencing the warning by explicit type declaration.

--- Engine/Code/Engine/Renderer/Renderer.hpp
+++ Engine/Code/Engine/Renderer/Renderer.hpp
@@ -63,7 +63,7 @@ public:
     : saveLocation{FileUtils::GetKnownFolderPath(FileUtils::KnownPathID::EngineData) / std::filesystem::path{"Screenshots"}} {
         namespace FS = std::filesystem;
         FileUtils::CreateFolders(saveLocation);
-        const auto folder = saveLocation;
+        const std::filesystem::path folder = saveLocation;
         const auto screenshot_count = FileUtils::CountFilesInFolders(folder);
         const auto filepath = folder / FS::path{"Screenshot_" + std::to_string(screenshot_count + 1) + ".png"};
         saveLocation = filepath;
