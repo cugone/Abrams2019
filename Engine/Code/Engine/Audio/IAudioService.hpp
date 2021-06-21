0023e60d2c634a6bf5f15c412fadbc8e56646956
cugone <cugone@gmail.com>
2021-06-20 22:59:27 -0500

    IAudioService Interface changed to noexcept

--- Engine/Code/Engine/Audio/IAudioService.hpp
+++ Engine/Code/Engine/Audio/IAudioService.hpp
@@ -8,14 +8,14 @@ public:
     virtual void SuspendAudio() noexcept = 0;
     virtual void ResumeAudio() noexcept = 0;
 
-    virtual void Play(const std::filesystem::path& filepath) = 0;
-    virtual void Play(const std::size_t id) = 0;
-    virtual void Play(const std::filesystem::path& filepath, const bool looping) = 0;
-    virtual void Play(const std::size_t id, const bool looping) = 0;
+    virtual void Play(const std::filesystem::path& filepath) noexcept = 0;
+    virtual void Play(const std::size_t id) noexcept = 0;
+    virtual void Play(const std::filesystem::path& filepath, const bool looping) noexcept = 0;
+    virtual void Play(const std::size_t id, const bool looping) noexcept = 0;
     
-    virtual void Stop(const std::filesystem::path& filepath) = 0;
-    virtual void Stop(const std::size_t id) = 0;
-    virtual void StopAll() = 0;
+    virtual void Stop(const std::filesystem::path& filepath) noexcept = 0;
+    virtual void Stop(const std::size_t id) noexcept = 0;
+    virtual void StopAll() noexcept = 0;
 protected:
 private:
 };
