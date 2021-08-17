05acc638c8484fba07cc2e906e902bd9f9d7cf2f
cugone <cugone@gmail.com>
2021-08-17 00:50:43 -0500

    Error if engine functions not called in correct order

--- Engine/Code/Engine/Core/EngineBase.hpp
+++ Engine/Code/Engine/Core/EngineBase.hpp
@@ -19,15 +19,19 @@ void RunMessagePump() noexcept;
 template<typename GameType>
 class Engine {
 public:
-    static_assert(std::is_base_of_v<std::remove_cv_t<std::remove_reference_t<std::remove_pointer_t<GameBase>>>, std::remove_cv_t<std::remove_reference_t<std::remove_pointer_t<GameType>>>>);
+    static_assert(std::is_base_of_v<std::remove_cv_t<std::remove_reference_t<std::remove_pointer_t<GameBase>>>, std::remove_cv_t<std::remove_reference_t<std::remove_pointer_t<GameType>>>>, "GameType template parameter is not derived from GameBase.");
     static void Initialize(const std::string& title, const std::string& cmdString) noexcept;
     static void Run() noexcept;
     static void Shutdown() noexcept;
+private:
+    static inline bool _initCalled{false};
+    static inline bool _shutdownCalled{false};
 };
 
 template<typename GameType>
 /*static*/
 void Engine<GameType>::Initialize(const std::string& title, const std::string& cmdString) noexcept {
+    _initCalled = true;
     App<GameType>::CreateApp(title, cmdString);
     auto& app = ServiceLocator::get<IAppService>();
     app.InitializeService();
@@ -36,6 +40,7 @@ void Engine<GameType>::Initialize(const std::string& title, const std::string& c
 template<typename GameType>
 /*static*/
 void Engine<GameType>::Run() noexcept {
+    GUARANTEE_OR_DIE(_initCalled, "Engine::Initialize not called before Run");
     auto& app = ServiceLocator::get<IAppService>();
     while(!app.IsQuitting()) {
         app.RunFrame();
@@ -45,5 +50,9 @@ void Engine<GameType>::Run() noexcept {
 template<typename GameType>
 /*static*/
 void Engine<GameType>::Shutdown() noexcept {
-    App<GameType>::DestroyApp();
+    GUARANTEE_OR_DIE(_initCalled, "Engine::Initialize not called before Shutdown");
+    if(!_shutdownCalled) {
+        _shutdownCalled = true;
+        App<GameType>::DestroyApp();
+    }
 }
