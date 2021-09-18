4418420b3311db123ebee1c1c0778a4d3a9278c1
cugone <cugone@gmail.com>
2021-09-18 11:06:19 -0500

    AppendFromFile only appends keys that don't already exist

--- Engine/Code/Engine/Core/Config.cpp
+++ Engine/Code/Engine/Core/Config.cpp
@@ -39,6 +39,9 @@ bool Config::AppendFromFile(const std::filesystem::path& filepath) noexcept {
         KeyValueParser kvp(filepath);
         const auto&& new_entries = std::move(kvp.Release());
         for(const auto& [key, value] : new_entries) {
+            if(HasKey(key)) {
+                continue;
+            }
             SetValue(key, value);
         }
         return true;
