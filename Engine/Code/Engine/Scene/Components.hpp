450f627d6566d5ce44252bfe71bb7878c8795b29
cugone <cugone@gmail.com>
2021-09-23 11:03:05 -0500

    Rule of Six

--- Engine/Code/Engine/Scene/Components.hpp
+++ Engine/Code/Engine/Scene/Components.hpp
@@ -11,8 +11,10 @@ struct TagComponent {
     TagComponent() noexcept = default;
     TagComponent(const TagComponent& other) noexcept = default;
     TagComponent(TagComponent&& r_other) noexcept = default;
+    TagComponent& operator=(const TagComponent& rhs) noexcept = default;
+    TagComponent& operator=(TagComponent&& rhs) noexcept = default;
+    ~TagComponent() noexcept = default;
     explicit TagComponent(const std::string& tag) noexcept : Tag{tag} {}
-
     operator const std::string&() const noexcept { return Tag; }
     operator std::string&() noexcept { return Tag; }
 };
@@ -23,10 +25,12 @@ struct TransformComponent {
     TransformComponent() noexcept = default;
     TransformComponent(const TransformComponent& other) noexcept = default;
     TransformComponent(TransformComponent&& r_other) noexcept = default;
+    TransformComponent& operator=(const TransformComponent& rhs) noexcept = default;
+    TransformComponent& operator=(TransformComponent&& rhs) noexcept = default;
     explicit TransformComponent(const Matrix4& transform) noexcept
     : Transform{transform} {
     }
-
+    ~TransformComponent() noexcept = default;
     operator const Matrix4&() const noexcept { return Transform; }
     operator Matrix4&() noexcept { return Transform; }
 
@@ -38,6 +42,9 @@ struct MeshComponent {
     MeshComponent() noexcept = default;
     MeshComponent(const MeshComponent& other) noexcept = default;
     MeshComponent(MeshComponent&& r_other) noexcept = default;
+    MeshComponent& operator=(const MeshComponent& rhs) noexcept = default;
+    MeshComponent& operator=(MeshComponent&& rhs) noexcept = default;
+    ~MeshComponent() noexcept = default;
     explicit MeshComponent(const Mesh& newMesh) noexcept
     : mesh{newMesh} {}
 
@@ -56,6 +63,9 @@ struct SceneComponent {
     SceneComponent() noexcept = default;
     SceneComponent(const SceneComponent& other) noexcept = default;
     SceneComponent(SceneComponent&& r_other) noexcept = default;
+    SceneComponent& operator=(const SceneComponent& rhs) noexcept = default;
+    SceneComponent& operator=(SceneComponent&& rhs) noexcept = default;
+    ~SceneComponent() noexcept = default;
     explicit SceneComponent(const TransformComponent& transform) noexcept
     : Transform{transform} {}
 
@@ -68,6 +78,9 @@ struct RenderComponent {
     RenderComponent() noexcept = default;
     RenderComponent(const RenderComponent& other) noexcept = default;
     RenderComponent(RenderComponent&& r_other) noexcept = default;
+    RenderComponent& operator=(const RenderComponent& rhs) noexcept = default;
+    RenderComponent& operator=(RenderComponent&& rhs) noexcept = default;
+    ~RenderComponent() noexcept = default;
     RenderComponent(std::string materialName, Rgba tint) noexcept
     : MaterialName{materialName}
     , Tint{tint} {}
