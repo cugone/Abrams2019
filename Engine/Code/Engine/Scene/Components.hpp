16b6f95f2ffe4d3c05296f921aa00ad0a21fb9b5
cugone <cugone@gmail.com>
2021-09-23 10:37:32 -0500

    Declare single-argument constructors of Components explicit

--- Engine/Code/Engine/Scene/Components.hpp
+++ Engine/Code/Engine/Scene/Components.hpp
@@ -11,7 +11,7 @@ struct TagComponent {
     TagComponent() noexcept = default;
     TagComponent(const TagComponent& other) noexcept = default;
     TagComponent(TagComponent&& r_other) noexcept = default;
-    TagComponent(const std::string& tag) noexcept : Tag{tag} {}
+    explicit TagComponent(const std::string& tag) noexcept : Tag{tag} {}
 
     operator const std::string&() const noexcept { return Tag; }
     operator std::string&() noexcept { return Tag; }
@@ -23,7 +23,7 @@ struct TransformComponent {
     TransformComponent() noexcept = default;
     TransformComponent(const TransformComponent& other) noexcept = default;
     TransformComponent(TransformComponent&& r_other) noexcept = default;
-    TransformComponent(const Matrix4& transform) noexcept
+    explicit TransformComponent(const Matrix4& transform) noexcept
     : Transform{transform} {
     }
 
@@ -38,7 +38,7 @@ struct MeshComponent {
     MeshComponent() noexcept = default;
     MeshComponent(const MeshComponent& other) noexcept = default;
     MeshComponent(MeshComponent&& r_other) noexcept = default;
-    MeshComponent(const Mesh& newMesh) noexcept
+    explicit MeshComponent(const Mesh& newMesh) noexcept
     : mesh{newMesh} {}
 
     operator const Mesh&() const noexcept {
@@ -56,7 +56,7 @@ struct SceneComponent {
     SceneComponent() noexcept = default;
     SceneComponent(const SceneComponent& other) noexcept = default;
     SceneComponent(SceneComponent&& r_other) noexcept = default;
-    SceneComponent(const TransformComponent& transform) noexcept
+    explicit SceneComponent(const TransformComponent& transform) noexcept
     : Transform{transform} {}
 
 };
