817ef1adb32c2133c8e69e97908d4b1a79edf5f4
cugone <cugone@gmail.com>
2021-09-27 12:56:19 -0500

    Add GetEntities... functions

--- Engine/Code/Engine/Scene/Scene.hpp
+++ Engine/Code/Engine/Scene/Scene.hpp
@@ -21,6 +21,29 @@ public:
     std::weak_ptr<const Scene> get() const noexcept;
     std::weak_ptr<Scene> get() noexcept;
 
+    const entt::registry& GetRegistry() const noexcept;
+    entt::registry& GetRegistry() noexcept;
+
+    template<typename Component>
+    decltype(auto) GetEntitiesWithComponent() const noexcept {
+        return m_registry.view<Component>();
+    }
+    
+    template<typename Component>
+    decltype(auto) GetEntitiesWithComponent() noexcept {
+        return m_registry.view<Component>();
+    }
+    
+    template<typename... Components>
+    decltype(auto) GetEntitiesWithComponents() const noexcept {
+        return m_registry.view<Components...>();
+    }
+    
+    template<typename... Components>
+    decltype(auto) GetEntitiesWithComponents() noexcept {
+        return m_registry.view<Components...>();
+    }
+
 protected:
 private:
     entt::registry m_registry{};
