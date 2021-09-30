8efe3be88c0b0788c6255e17debca723d5554719
cugone <cugone@gmail.com>
2021-09-29 23:57:01 -0500

    Declare random parts of UUID thread_local

--- Engine/Code/Engine/Scene/UUID.cpp
+++ Engine/Code/Engine/Scene/UUID.cpp
@@ -5,8 +5,8 @@
 #include <random>
 
 UUID::UUID() noexcept {
-    static std::random_device rd{};
-    static std::mt19937_64 e{rd()};
+    static thread_local std::random_device rd{};
+    static thread_local std::mt19937_64 e{rd()};
     id = std::uniform_int_distribution<uint64_t>{}(e);
 }
 
