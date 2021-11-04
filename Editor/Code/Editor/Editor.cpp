b04d0313829b4753fc01793b2a17032e95253547
cugone <cugone@gmail.com>
2021-11-03 23:26:50 -0500

    Refactor UI functions

--- Editor/Code/Editor/Editor.cpp
+++ Editor/Code/Editor/Editor.cpp
@@ -5,10 +5,13 @@
 #include "Engine/Core/EngineCommon.hpp"
 #include "Engine/Core/EngineConfig.hpp"
 
+#include "Engine/Platform/DirectX/DirectX11FrameBuffer.hpp"
 #include "Engine/Platform/PlatformUtils.hpp"
 #include "Engine/Platform/Win.hpp"
 
 #include "Engine/Renderer/Renderer.hpp"
+
+#include "Engine/Scene/Scene.hpp"
 
 #include "Engine/Services/ServiceLocator.hpp"
 #include "Engine/Services/IAppService.hpp"
@@ -20,6 +23,7 @@
 
 #include <algorithm>
 #include <numeric>
+#include <sstream>
 
 void Editor::UpdateContentBrowserPaths(std::vector<std::filesystem::path>& cache) {
     cache.clear();
@@ -34,13 +38,26 @@ void Editor::PollContentBrowserPaths(std::vector<std::filesystem::path>& cache) 
     }
 };
 
+void Editor::ShowSelectedEntityComponents(TimeUtils::FPSeconds deltaSeconds) noexcept {
+    /* DO NOTHING */
+}
+
+void Editor::HandleInput(TimeUtils::FPSeconds deltaSeconds) noexcept {
+    HandleMenuKeyboardInput(deltaSeconds);
+    HandleCameraInput(deltaSeconds);
+}
+
+bool Editor::IsSceneLoaded() const noexcept {
+    return m_ActiveScene.get() != nullptr;
+}
 
 void Editor::Initialize() noexcept {
     auto& renderer = ServiceLocator::get<IRendererService>();
     m_ContentBrowserCurrentDirectory = FileUtils::GetKnownFolderPath(FileUtils::KnownPathID::GameData);
     renderer.RegisterTexturesFromFolder(m_ContentBrowserCurrentDirectory / std::filesystem::path{"Images"}, true);
-    renderer.RegisterTexturesFromFolder(m_ContentBrowserCurrentDirectory / std::filesystem::path{"Icons"}, true);
+    renderer.RegisterTexturesFromFolder(m_ContentBrowserCurrentDirectory / std::filesystem::path{"Resources/Icons"}, true);
     UpdateContentBrowserPaths(m_ContentBrowserPathsCache);
+    buffer = FrameBuffer::Create(FrameBufferDesc{});
 }
 
 void Editor::BeginFrame() noexcept {
@@ -48,40 +65,25 @@ void Editor::BeginFrame() noexcept {
 }
 
 void Editor::Update(TimeUtils::FPSeconds deltaSeconds) noexcept {
-    ImGui::BeginMainMenuBar();
-    {
-        if(ImGui::BeginMenu("File")) {
-            if(ImGui::MenuItem("New", "Ctrl+N")) {
-                DoFileNew();
-            }
-            if(ImGui::MenuItem("Open...", "Ctrl+O")) {
-                DoFileOpen();
-            }
-            ImGui::Separator();
-            if(ImGui::MenuItem("Save", "Ctrl+S", nullptr, m_ActiveScene.get())) {
-                DoFileSave();
-            }
-            if(ImGui::MenuItem("Save As...", "", nullptr, m_ActiveScene.get())) {
-                DoFileSaveAs();
-            }
-            if(ImGui::MenuItem("Exit")) {
-                auto& app = ServiceLocator::get<IAppService>();
-                app.SetIsQuitting(true);
-            }
-            ImGui::EndMenu();
-        }
-        ImGui::EndMainMenuBar();
-    }
-    ShowContentBrowserWindow();
-    ShowMainViewport();
-    HandleMenuKeyboardInput();
+    ShowUI(deltaSeconds);
+    HandleInput(deltaSeconds);
 }
 
 void Editor::Render() const noexcept {
 
     auto& renderer = ServiceLocator::get<IRendererService>();
 
-    //renderer.BeginRender(buffer->GetTexture(), Rgba::Black, buffer->GetDepthStencil());
+    renderer.BeginRender(buffer->GetTexture(), Rgba::Black, buffer->GetDepthStencil());
+
+    renderer.SetOrthoProjectionFromCamera(Camera3D{m_editorCamera.GetCamera()});
+    renderer.SetCamera(m_editorCamera.GetCamera());
+
+    const auto S = Matrix4::I;
+    const auto R = Matrix4::I;
+    const auto T = Matrix4::I;
+    const auto M = Matrix4::MakeSRT(S, R, T);
+    renderer.SetMaterial("__2D");
+    renderer.DrawQuad2D(M, Rgba::Green);
 
     renderer.BeginRenderToBackbuffer();
 
@@ -105,7 +107,7 @@ void Editor::DoFileNew() noexcept {
 
 void Editor::DoFileOpen() noexcept {
     if(auto path = FileDialogs::OpenFile("Abrams Scene (*.abr)\0*.abr\0All Files (*.*)\0*.*\0\0"); !path.empty()) {
-
+        
     }
 }
 
@@ -118,13 +120,100 @@ void Editor::DoFileSave() noexcept {
     /* DO NOTHING */
 }
 
-void Editor::ShowMainViewport() noexcept {
-    //ImGui::Begin("SceneViewport", nullptr, ImGuiWindowFlags_NoTitleBar);
-    //ImGui::Image(buffer->GetTexture(), ImGui::GetWindowSize(), Vector2::Zero, Vector2::One, Rgba::NoAlpha, Rgba::NoAlpha);
-    //ImGui::End();
+void Editor::ShowUI(TimeUtils::FPSeconds deltaSeconds) noexcept {
+    ShowMainMenu(deltaSeconds);
+    ShowSettingsWindow(deltaSeconds);
+    ShowPropertiesWindow(deltaSeconds);
+    ShowContentBrowserWindow(deltaSeconds);
+    ShowMainViewport(deltaSeconds);
 }
 
-void Editor::ShowContentBrowserWindow() noexcept {
+static int cameraSpeedMultiplier = 1;
+
+void Editor::ShowMainMenu(TimeUtils::FPSeconds deltaSeconds) noexcept {
+    ImGui::BeginMainMenuBar();
+    {
+        if(ImGui::BeginMenu("File")) {
+            if(ImGui::MenuItem("New", "Ctrl+N")) {
+                DoFileNew();
+            }
+            if(ImGui::MenuItem("Open...", "Ctrl+O")) {
+                DoFileOpen();
+            }
+            ImGui::Separator();
+            if(ImGui::MenuItem("Save", "Ctrl+S", nullptr, m_ActiveScene.get())) {
+                DoFileSave();
+            }
+            if(ImGui::MenuItem("Save As...", "", nullptr, m_ActiveScene.get())) {
+                DoFileSaveAs();
+            }
+            if(ImGui::MenuItem("Exit")) {
+                auto& app = ServiceLocator::get<IAppService>();
+                app.SetIsQuitting(true);
+            }
+            ImGui::EndMenu();
+        }
+        if(ImGui::BeginMenu("Debug")) {
+            if(ImGui::MenuItem("Dear ImGui Demo Window", nullptr, g_theUISystem->IsImguiDemoWindowVisible(), IsDebuggerAvailable())) {
+                g_theUISystem->ToggleImguiDemoWindow();
+            }
+            if(ImGui::MenuItem("Dear ImGui Metrics Window", nullptr, g_theUISystem->IsImguiMetricsWindowVisible(), IsDebuggerAvailable())) {
+                g_theUISystem->ToggleImguiMetricsWindow();
+            }
+            ImGui::EndMenu();
+        }
+        if(ImGui::BeginMenu("Camera")) {
+            if(ImGui::BeginMenu("Camera Speed")) {
+                if(ImGui::SliderInt("Speed##CameraSpeedSlider", &cameraSpeedMultiplier, 1, 10, "%d", ImGuiSliderFlags_AlwaysClamp | ImGuiSliderFlags_NoInput)) {
+                    m_editorCamera.SetSpeedMultiplier(cameraSpeedMultiplier);
+                }
+                ImGui::EndMenu();
+            }
+            ImGui::EndMenu();
+        }
+        ImGui::EndMainMenuBar();
+    }
+}
+
+void Editor::ShowSettingsWindow(TimeUtils::FPSeconds deltaSeconds) noexcept {
+    ImGui::Begin("Settings");
+    {
+        ShowSelectedEntityComponents(deltaSeconds);
+    }
+    ImGui::End();
+}
+
+void Editor::ShowPropertiesWindow([[maybe_unused]] TimeUtils::FPSeconds deltaSeconds) noexcept {
+    ImGui::Begin("Properties");
+    {
+
+    }
+    ImGui::End();
+}
+
+void Editor::ShowMainViewport(TimeUtils::FPSeconds deltaSeconds) noexcept {
+    {
+        std::ostringstream ss{};
+        if(IsSceneLoaded()) {
+            //TODO: Scenes should store their names.
+            ss << m_ActiveScene;
+        } else {
+            ss << "Viewport";
+        }
+        ImGui::Begin(ss.str().c_str(), nullptr);
+    }
+    const auto viewportSize = ImGui::GetContentRegionAvail();
+    if(viewportSize.x != m_ViewportWidth || viewportSize.y != m_ViewportHeight) {
+        m_ViewportWidth = static_cast<uint32_t>(std::floor(viewportSize.x));
+        m_ViewportHeight = static_cast<uint32_t>(std::floor(viewportSize.y));
+        buffer->Resize(m_ViewportWidth, m_ViewportHeight);
+    }
+    ImGui::Image(buffer->GetTexture(), viewportSize, Vector2::Zero, Vector2::One, Rgba::White, Rgba::NoAlpha);
+    m_IsViewportWindowActive = ImGui::IsWindowFocused() || ImGui::IsWindowHovered();
+    ImGui::End();
+}
+
+void Editor::ShowContentBrowserWindow(TimeUtils::FPSeconds deltaSeconds) noexcept {
     if(m_CacheNeedsImmediateUpdate) {
         UpdateContentBrowserPaths(m_ContentBrowserPathsCache);
     } else {
@@ -140,13 +229,19 @@ void Editor::ShowContentBrowserWindow() noexcept {
             }
         }
         const auto padding = 16.0f;
-        //TODO: Add global UI scaling
-        static auto scale = 1.0f;
+        const auto& config = ServiceLocator::get<IConfigService>();
+        static auto scale = 0.5f;
+        if(config.HasKey("UIScale")) {
+            config.GetValue("UIScale", scale);
+        }
         scale = std::clamp(scale, 0.125f, 2.0f);
-        const auto thumbnailSize = std::max(32.0f, 256.0f * scale);
+        const auto thumbnailSize = (std::max)(32.0f, 256.0f * scale);
         const auto cellSize = thumbnailSize + padding;
-        const auto panelWidth = ImGui::GetContentRegionAvail().x;
-        const auto columnCount = std::min(std::max(1, static_cast<int>(panelWidth / cellSize)), 64);
+        const auto panelWidth = static_cast<uint32_t>(std::floor(ImGui::GetContentRegionAvail().x));
+        if(panelWidth != m_ContentBrowserPanelWidth) {
+            m_ContentBrowserPanelWidth = panelWidth;
+        }
+        const auto columnCount = (std::min)((std::max)(1, static_cast<int>(m_ContentBrowserPanelWidth / cellSize)), 64);
         ImGui::BeginTable("##ContentBrowser", columnCount, ImGuiTableFlags_SizingFixedFit | ImGuiTableFlags_ContextMenuInBody);
         {
             auto id = 0u;
@@ -168,7 +263,9 @@ void Editor::ShowContentBrowserWindow() noexcept {
                 ImGui::PopID();
                 ImGui::PopStyleColor();
                 ImGui::PopStyleColor();
-                ImGui::TextWrapped(p.filename().string().c_str());
+                const auto filename_string = p.filename().string();
+                const auto filename_size = ImGui::CalcTextSize(filename_string.c_str(), nullptr);
+                ImGui::TextWrapped(filename_string.c_str());
                 ImGui::EndGroup();
             }
         }
@@ -177,7 +274,7 @@ void Editor::ShowContentBrowserWindow() noexcept {
     ImGui::End();
 }
 
-void Editor::HandleMenuKeyboardInput() noexcept {
+void Editor::HandleMenuKeyboardInput(TimeUtils::FPSeconds deltaSeconds) noexcept {
     auto& input = ServiceLocator::get<IInputService>();
     if(input.IsKeyDown(KeyCode::Ctrl)) {
         if(input.WasKeyJustPressed(KeyCode::N)) {
@@ -187,6 +284,12 @@ void Editor::HandleMenuKeyboardInput() noexcept {
         } else if(input.WasKeyJustPressed(KeyCode::S)) {
             DoFileSaveAs();
         }
+    }
+}
+
+void Editor::HandleCameraInput(TimeUtils::FPSeconds deltaSeconds) noexcept {
+    if(m_IsViewportWindowActive) {
+        m_editorCamera.Update(deltaSeconds);
     }
 }
 
@@ -206,21 +309,15 @@ Texture* Editor::GetAssetTextureFromType(const std::filesystem::path& path) cons
             p = p.make_preferred();
             return p;
         };
-        const auto IsImageAsset = [&](const std::filesystem::path& ext) {
-            for(const auto& p : StringUtils::Split((Image::GetSupportedExtensionsList()))) {
-                if(p == ext.string()) return true;
-            }
-            return false;
-        };
         if(std::filesystem::is_directory(path)) {
-            p = BuildPath("Icons/FolderAsset.png");
+            p = BuildPath("Resources/Icons/FolderAsset.png");
         } else if(e == ".txt") {
-            p = BuildPath("Icons/TextAsset.png");
+            p = BuildPath("Resources/Icons/TextAsset.png");
         } else if(e == ".ascene") {
-            p = BuildPath("Icons/SceneAsset.png");
+            p = BuildPath("Resources/Icons/SceneAsset.png");
         } else if(e == ".log") {
-            p = BuildPath("Icons/LogAsset.png");
-        } else if(IsImageAsset(e)) {
+            p = BuildPath("Resources/Icons/LogAsset.png");
+        } else if(IsImageAssetExtension(e)) {
             return renderer.CreateOrGetTexture(path, IntVector3::XY_Axis);
         }
         return p.empty() ? defaultTexture : renderer.GetTexture(p.string());
@@ -236,11 +333,16 @@ bool Editor::IsAssetExtension(const std::filesystem::path& ext) const noexcept {
     } else if(ext == ".log") {
         return true;
     } else {
-        const auto& supportedExtensions = StringUtils::Split(Image::GetSupportedExtensionsList());
-        if(std::find(std::cbegin(supportedExtensions), std::cend(supportedExtensions), ext.string()) != std::cend(supportedExtensions)) {
+        return IsImageAssetExtension(ext);
+    }
+}
+
+bool Editor::IsImageAssetExtension(const std::filesystem::path& ext) const noexcept {
+    static const auto ext_string = Image::GetSupportedExtensionsList();
+    static const auto ext_list = StringUtils::Split(ext_string);
+    for(const auto& p : ext_list) {
+        if(p == ext.string()) {
             return true;
-        } else {
-            return false;
         }
     }
     return false;
--- Editor/Code/Editor/Editor.hpp
+++ Editor/Code/Editor/Editor.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include "Engine/Core/OrthographicCameraController.hpp"
 #include "Engine/Core/Stopwatch.hpp"
+
 #include "Engine/Game/GameBase.hpp"
 
 #include "Engine/Renderer/FrameBuffer.hpp"
@@ -12,6 +14,7 @@ class Texture;
 
 class Editor : public GameBase {
 public:
+
     void Initialize() noexcept override;
     void BeginFrame() noexcept override;
     void Update(TimeUtils::FPSeconds deltaSeconds) noexcept override;
@@ -23,27 +26,42 @@ public:
 
 protected:
 private:
+
+    void ShowUI([[maybe_unused]] TimeUtils::FPSeconds deltaSeconds) noexcept;
+    void ShowMainMenu([[maybe_unused]] TimeUtils::FPSeconds deltaSeconds) noexcept;
+    void ShowSettingsWindow([[maybe_unused]] TimeUtils::FPSeconds deltaSeconds) noexcept;
+    void ShowSelectedEntityComponents([[maybe_unused]] TimeUtils::FPSeconds deltaSeconds) noexcept;
+    void ShowPropertiesWindow([[maybe_unused]] TimeUtils::FPSeconds deltaSeconds) noexcept;
+    void ShowMainViewport([[maybe_unused]] TimeUtils::FPSeconds deltaSeconds) noexcept;
+    void ShowContentBrowserWindow([[maybe_unused]] TimeUtils::FPSeconds deltaSeconds) noexcept;
+    void HandleInput([[maybe_unused]] TimeUtils::FPSeconds deltaSeconds) noexcept;
+    void HandleMenuKeyboardInput([[maybe_unused]] TimeUtils::FPSeconds deltaSeconds) noexcept;
+    void HandleCameraInput([[maybe_unused]] TimeUtils::FPSeconds deltaSeconds) noexcept;
+
     void DoFileNew() noexcept;
     void DoFileOpen() noexcept;
     void DoFileSaveAs() noexcept;
     void DoFileSave() noexcept;
 
-    void ShowMainViewport() noexcept;
-    void ShowContentBrowserWindow() noexcept;
-    void HandleMenuKeyboardInput() noexcept;
+    bool IsSceneLoaded() const noexcept;
 
     bool HasAssetExtension(const std::filesystem::path& path) const noexcept;
     bool IsAssetExtension(const std::filesystem::path& ext) const noexcept;
+    bool IsImageAssetExtension(const std::filesystem::path& ext) const noexcept;
     Texture* GetAssetTextureFromType(const std::filesystem::path& path) const noexcept;
-
 
     void UpdateContentBrowserPaths(std::vector<std::filesystem::path>& cache);
     void PollContentBrowserPaths(std::vector<std::filesystem::path>& cache);
 
     std::filesystem::path m_ContentBrowserCurrentDirectory{};
 
+    OrthographicCameraController m_editorCamera{};
     Stopwatch m_ContentBrowserUpdatePoll{1.0f};
     std::vector<std::filesystem::path> m_ContentBrowserPathsCache{};
+    uint32_t m_ContentBrowserPanelWidth{1600u};
+    uint32_t m_ViewportWidth{1600u};
+    uint32_t m_ViewportHeight{900u};
     bool m_CacheNeedsImmediateUpdate{true};
-    //std::unique_ptr<FrameBuffer> buffer{};
+    bool m_IsViewportWindowActive{false};
+    std::shared_ptr<FrameBuffer> buffer{};
 };
